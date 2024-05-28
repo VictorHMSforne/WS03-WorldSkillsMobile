97a88ec2c18cdebf96ea22776184b9acbc5e3d7d
Aluno <Aluno@SNCTM202LABF03>
2024-05-28 14:53:07 -0300

    Adicionar arquivos de projeto.

---
+++ WS03/WS03/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using WS03.Servico; // adicionei
+using WS03.Servico.Modelo;// adicionei
+
+namespace WS03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnCEP.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = txtCEP.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        lblResultado.Text = string.Format("Endereço: {2},{3},{0},{1}.", end.localidade, end.uf, end.logradouro, end.bairro);
+                        txtCEP.Text = "";
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "CEP Não Encontrado!", "Ok");
+                    }
+                }
+                catch (Exception erro) 
+                {
+                    DisplayAlert("Erro", erro.Message, "Ok");
+                }
+            }
+        }
+
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP inválido! O CEP tem apenas 8 números", "Ok");
+                valido = false;
+            }
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("Erro", "O CEP contém apenas números", "Ok");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ WS03/WS03/Servico/ViaCEPServico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using WS03.Servico.Modelo; //adicionei
+using Newtonsoft.Json; //adicionei
+using System.Net;
+
+namespace WS03.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/"; // esse cara so usa nessa classe
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL); //vai baixar aquele array
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+            if (end.cep == null) return null;
+            return end;
+        }
+    }
+}
